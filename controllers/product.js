40e4c6750fa7179e64050a46294899a9e55c1dae
Tochukwu Idoko <idokotheodore@gmail.com>
2021-07-11 08:31:49 +0200

    other-additions

--- controllers/product.js
+++ controllers/product.js
@@ -90,13 +90,13 @@ exports.update = (req, res, next) => {
     }
 
     // check for all fields
-    const {name, description, price, category, quantity, shipping} = fields
+    //const {name, description, price, category, quantity, shipping} = fields
 
-    if(!name || !description || !price || !category  || !quantity || !shipping) {
-      return res.status(400).json({
-        error:'All fields are required'
-      })
-    }
+    //if(!name || !description || !price || !category  || !quantity || !shipping) {
+      //return res.status(400).json({
+        //error:'All fields are required'
+      //})
+   // }
     // update product
     let product = req.product;
     product = _.extend(product, fields);
